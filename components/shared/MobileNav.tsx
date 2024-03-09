e8c01259df6b9bd3b09077bce54c591b97560d64
Raghav Goel <goelraghav002@gmail.com>
2024-03-09 21:23:59 +0530

    Added User button on mobile view

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -67,6 +67,9 @@ const MobileNav = () => {
                                                 </li>
                                             )
                                         })}
+                                    <li className='flex-center cursor-pointer gap-2 p-4'>
+                                        <UserButton afterSignOutUrl='/' showName />
+                                    </li>
                                 </ul>
                             </>
                         </SheetContent>
